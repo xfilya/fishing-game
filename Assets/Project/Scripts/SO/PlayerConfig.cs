640a0987edd4d7bf3e92dc755f5b2a32ef617e52
Maxim <filimonov095@mail.ru>
2026-09-08 01:49:50 +0300

    прописал базовый PlayerConfig SO

---
+++ Assets/Project/Scripts/SO/PlayerConfig.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerConfig", menuName = "Configs/PlayerConfig")]
+public class PlayerConfig : ScriptableObject
+{
+    [Header("Movement")]
+    [field: SerializeField] public float BaseSpeed { get; private set; } = 5f;
+    [field: SerializeField] public float Gravity { get; private set; } = -20f;
+
+    [Header("Camera")]
+    [field: SerializeField] public float MouseSensitivity { get; private set; } = 0.1f;
+    [field: SerializeField] public float StickSensitivity { get; private set; } = 180f;
+    [field: SerializeField] public float CameraClampAngle { get; private set; } = 89f;   
+}
